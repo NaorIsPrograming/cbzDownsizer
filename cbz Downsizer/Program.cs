efdaffd52ea59f29c361f1d8639b5abb88174d33
NaorIsPrograming <213268147naor@gmail.com>
2023-05-05 14:52:46 +0300

    initializing repository

---
+++ cbz Downsizer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace cbz_Downsizer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
